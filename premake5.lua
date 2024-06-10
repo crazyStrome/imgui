c36181858a81d55afcbd7b81098f255ee5966b57
haandy <1415441419@qq.com>
2024-06-10 21:16:45 +0800

    premake

--- premake5.lua
+++ premake5.lua
@@ -18,8 +18,14 @@ project "ImGui"
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
 		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"imgui_demo.cpp",
+        "backends/imgui_impl_glfw.cpp",
+        "backends/imgui_impl_opengl3.cpp",
 	}
+    
+    includedirs {
+        "%{Includedir.ImGui}",
+    }
 
 	filter "system:windows"
 		systemversion "latest"
